90fd8dcc23903f63762d035fc86e337936f2ff3d
Andre S. <andre.simoesdutra23@gmail.com>
2024-11-03 14:43:02 -0300

    bitcoin endpoint

--- src/Backend/Api/server.ts
+++ src/Backend/Api/server.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const PORT: number = 8023; 
+const PORT: number = 8023;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!")
-})
+});
+
+app.get("/bitcoin", (req: Request, res: Response) => {
+    res.send("O bitcoin está custando: $395.213");
+});
 
 app.listen(PORT, () => {
     console.log(`\nRunning server on: http://localhost:${PORT}\n`)
